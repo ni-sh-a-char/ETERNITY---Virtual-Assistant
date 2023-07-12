0da89ebb60ed4340e4fb05c72a9c83917517fab2
PIYUSH MISHRA <65788721+PIYUSH-MISHRA-00@users.noreply.github.com>
2023-07-12 09:49:52 +0530

    Update ETERNITY.py
    integrated E.T.E.R.GPT

--- ETERNITY.py
+++ ETERNITY.py
@@ -1,289 +1,265 @@
-from encodings import search_function
-from cv2 import split
-from jmespath import search
-from numpy import void
-from time import ctime # get time details
-import speech_recognition as sr # to recognize speech
-import playsound # for playing an audio
-import random 
-from gtts import gTTS # google text to speech
-import webbrowser # to open browser
-import ssl
-import certifi
-import time 
-import os # for removing the audio files when necessary
-import subprocess
-from PIL import Image
-import pyautogui # for screenshot
+import random
+import webbrowser
 import pyttsx3
-import bs4 as bs
-import urllib.request
+import speech_recognition as sr
+from gtts import gTTS
+import playsound
+import openai
+import pyaudio
+import time
+import pyautogui
 
-from webob import minute
+# Initialize OpenAI API
+openai.api_key = "Paste your OpenAI key"
 
-class person:
-    name = ''
-    def setName(self, name):
-        self.name = name
+# Initialize the text to speech engine
+engine = pyttsx3.init()
 
-class asis:
-    name = ''
-    def setName(self, name):
-        self.name = name
+interrupt_flag = False  # Flag to indicate if interrupt was triggered
 
-def there_exists(terms):
-    for term in terms:
-        if term in voice_data:
-            return True
+def transcribe_audio_to_text(filename):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+    try:
+        return recognizer.recognize_google(audio)
+    except:
+        print("Skipping unknown error")
 
-def engine_speak(text):
-    text = str(text)
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=4000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response["choices"][0]["text"]
+
+def speak_text(text):
     engine.say(text)
     engine.runAndWait()
 
-r = sr.Recognizer() # initialise a recogniser
-# listen for audio and convert it to text:
 def record_audio(ask=""):
-    with sr.Microphone() as source: # microphone as source
+    global interrupt_flag
+
+    with sr.Microphone() as source:
         if ask:
-            engine_speak(ask)
-        audio = r.listen(source, 5, 5)  # listen for the audio via source
-        print("Okh")
+            speak_text(ask)
+        recognizer = sr.Recognizer()
+        audio = recognizer.listen(source, 5, 5)
         voice_data = ''
+
+        if interrupt_flag:  # Check if interrupt flag is True
+            interrupt_flag = False
+            return ''
+
         try:
-            voice_data = r.recognize_google(audio)  # convert audio to text
-        except sr.UnknownValueError: # error: recognizer does not understand
-            engine_speak("Sorry...")
+            voice_data = recognizer.recognize_google(audio)
+        except sr.UnknownValueError:
+            speak_text("Sorry, I didn't catch that.")
         except sr.RequestError:
-            engine_speak("Sorry I listened it but cant process as net is down") # error recognizer is not connected
-        print(">>", voice_data.lower()) # print what user said
+            speak_text("Sorry, I can't process your request at the moment.")
+
+        print(">>", voice_data.lower())
         return voice_data.lower()
 
-# get string and make a audio file to be played
-def engine_speak(audio_string):
-    audio_string = str(audio_string)
-    tts = gTTS(text=audio_string, lang='en') # text to speech(voice)
-    r = random.randint(1,20000000)
-    audio_file = 'audio' + str(r) + '.mp3'
-    tts.save(audio_file) # save as mp3
-    playsound.playsound(audio_file) # play the audio file
-    print(asis_obj.name + ":", audio_string) # print what app said
-    os.remove(audio_file) # remove audio file
-
 def respond(voice_data):
+    global engine, interrupt_flag
+
+    if "mute" in voice_data or "cancel" in voice_data or "stop" in voice_data:
+        interrupt_flag = True
+        engine.stop()
+        return
+
     # 1: greeting
-    if there_exists(['hello', 'hi', 'hey', 'hey miss', 'hola', 'hallo']):
-        greetings = ["yeah, how can I help you" + person_obj.name, "hey, what's up?" + person_obj.name, "I'm listening" + person_obj.name, "how can I help you?" + person_obj.name, "hello" + person_obj.name]
-        greet = greetings[random.randint(0,len(greetings)-1)]
-        engine_speak(greet)
+
+    if "hello" in voice_data or "hi" in voice_data or "hey" in voice_data:
+        greetings = [
+            "Yeah, how can I help you, " + person_obj.name + "?",
+            "Hey, what's up, " + person_obj.name + "?",
+            "I'm listening, " + person_obj.name + ".",
+            "How can I assist you, " + person_obj.name + "?",
+            "Hello, " + person_obj.name + ".",
+        ]
+        greet = random.choice(greetings)
+        speak_text(greet)
 
     # 2: name
-    if there_exists(["what is your name","what's your name","tell me your name"]):
+    if "what is your name" in voice_data or "what's your name" in voice_data or "tell me your name" in voice_data:
         if person_obj.name:
-            engine_speak("whats with my name ")
+            speak_text("What's with my name?")
         else:
-            engine_speak("i dont know my name . what's your name?")
+            speak_text("I don't know my name. What's your name?")
 
-    if there_exists(["my name is"]):
+    if "my name is" in voice_data:
         person_name = voice_data.split("is")[-1].strip()
-        engine_speak("okh, i will remember that " + person_name)
-        person_obj.setName(person_name) # remember name in person object
-    
-    if there_exists(["your name should be"]):
+        speak_text("Ok, I will remember that " + person_name)
+        person_obj.setName(person_name)
+
+    if "your name should be" in voice_data:
         asis_name = voice_data.split("be")[-1].strip()
-        engine_speak("okh, i will remember that's my name is " + asis_name)
-        asis_obj.setName(asis_name) # remember name in asis object
+        speak_text("Ok, I will remember that's my name is " + asis_name)
+        asis_obj.setName(asis_name)
 
     # 3: greeting
-    if there_exists(["how are you","how are you doing"]):
-        engine_speak("I'm very well, thanks for asking " + person_obj.name)
+    if "how are you" in voice_data or "how are you doing" in voice_data:
+        speak_text("I'm very well, thanks for asking " + person_obj.name)
 
     # 4: time
-    if there_exists(["what's the time","tell me the time","what time is it"]):
-        time = ctime().split(" ")[3].split(":")[0:2]
-        if time[0] == "00":
+    if "what's the time" in voice_data or "tell me the time" in voice_data or "what time is it" in voice_data:
+        current_time = time.ctime().split(" ")[3].split(":")[0:2]
+        if current_time[0] == "00":
             hours = '12'
         else:
-            hours = time[0]
-        minutes = time[1]
-        time = hours + " hours and " + minutes + "minutes"
-        engine_speak(time)
+            hours = current_time[0]
+        minutes = current_time[1]
+        speak_text(hours + " hours and " + minutes + " minutes")
 
     # 5: search google
-    if there_exists(["search for"]) and 'youtube' not in voice_data:
+    if there_exists(["search for"], voice_data) and 'youtube' not in voice_data:
         search_term = voice_data.split("for")[-1]
         url = "https://google.com/search?q=" + search_term
         webbrowser.get().open(url)
-        engine_speak("Here is what I found for" + search_term + "on google")
+        speak_text("Here is what I found for " + search_term + " on Google.")
 
     # 6: search youtube
-    if there_exists(["youtube"]):
+    if "youtube" in voice_data:
         search_term = voice_data.split("for")[-1]
         url = "https://www.youtube.com/results?search_query=" + search_term
         webbrowser.get().open(url)
-        engine_speak("Here is what I found for " + search_term + "on youtube")
+        speak_text("Here is what I found for " + search_term + " on YouTube.")
 
-    #7: get stock price
-    if there_exists(["price of"]):
+    # 7: get stock price
+    if there_exists(["price of"], voice_data):
         search_term = voice_data.split("for")[-1]
         url = "https://google.com/search?q=" + search_term
         webbrowser.get().open(url)
-        engine_speak("Here is what I found for " + search_term + " on google")
-    
-    # search for music
-    if there_exists(["play music"]):
-        search_term= voice_data.split("for")[-1]
-        url="https://open.spotify.com/search/"+search_term
-        webbrowser.get().open(url)
-        engine_speak("You are listening to"+ search_term +"enjoy")
-    #search for amazon.com
-    if there_exists(["amazon.com"]):
-        search_term = voice_data.split("for")[-1]
-        url="https://www.amazon.in"+search_term
-        webbrowser.get().open(url)
-        engine_speak("here is what i found for"+search_term + "on amazon.com")
-         
-    #make a note
-    if there_exists(["make a note"]):
-        search_term=voice_data.split("for")[-1]
-        url="https://keep.google.com/#home"
-        webbrowser.get().open(url)
-        engine_speak("Here you can make notes")
-        
-    #open instagram
-    if there_exists(["open insta"]):
-        search_term=voice_data.split("for")[-1]
-        url="https://www.instagram.com/"
-        webbrowser.get().open(url)
-        engine_speak("opening instagram")
-        
-    #open twitter
-    if there_exists(["i want tweets"]):
-        search_term=voice_data.split("for")[-1]
-        url="https://twitter.com/"
-        webbrowser.get().open(url)
-        engine_speak("opening twitter")
+        speak_text("Here is what I found for " + search_term + " on Google.")
 
-    # open Galiyaara
-    if there_exists(["open galiyara"]):
+    # 8: play music
+    if there_exists(["play music"], voice_data):
+        search_term = voice_data.split("for")[-1]
+        url = "https://open.spotify.com/search/" + search_term
+        webbrowser.get().open(url)
+        speak_text("You are listening to " + search_term + ". Enjoy!")
+
+    # 9: open a website
+    if there_exists(["open a website"], voice_data):
+        website = voice_data.split("website")[-1].strip()
+        url = "https://" + website
+        webbrowser.get().open(url)
+        speak_text("Opening " + website)
+
+    # 10: make a note
+    if there_exists(["make a note"], voice_data):
+        search_term = voice_data.split("for")[-1]
+        url = "https://keep.google.com/#home"
+        webbrowser.get().open(url)
+        speak_text("Here you can make notes.")
+
+    # 11: open Instagram
+    if there_exists(["open Instagram"], voice_data):
+        search_term = voice_data.split("for")[-1]
+        url = "https://www.instagram.com/"
+        webbrowser.get().open(url)
+        speak_text("Opening Instagram.")
+
+    # 12: open Twitter
+    if there_exists(["open Twitter"], voice_data):
+        search_term = voice_data.split("for")[-1]
+        url = "https://twitter.com/"
+        webbrowser.get().open(url)
+        speak_text("Opening Twitter.")
+
+    # 13: open Galiyaara
+    if there_exists(["open Galiyaara"], voice_data):
         search_term = voice_data.split("for")[-1]
         url = "https://galiyaara-36629.web.app/"
         webbrowser.get().open(url)
-        engine_speak("here's your galiyaara")
-        
-    #8 time table
-    if there_exists(["show my time table"]):
-        im = Image.open(r"image.jpeg")
+        speak_text("Here's your Galiyaara.")
+
+    # 14: show time table
+    if there_exists(["show my time table"], voice_data):
+        im = Image.open("image.jpeg")
         im.show()
-    
-    #9 weather
-    if there_exists(["weather","tell me the weather report","whats the condition outside"]):
+
+    # 15: weather
+    if there_exists(["weather", "tell me the weather report", "what's the condition outside"], voice_data):
         search_term = voice_data.split("for")[-1]
-        url = "https://www.google.com/search?sxsrf=ACYBGNSQwMLDByBwdVFIUCbQqya-ET7AAA%3A1578847393212&ei=oUwbXtbXDN-C4-EP-5u82AE&q=weather&oq=weather&gs_l=psy-ab.3..35i39i285i70i256j0i67l4j0i131i67j0i131j0i67l2j0.1630.4591..5475...1.2..2.322.1659.9j5j0j1......0....1..gws-wiz.....10..0i71j35i39j35i362i39._5eSPD47bv8&ved=0ahUKEwiWrJvwwP7mAhVfwTgGHfsNDxsQ4dUDCAs&uact=5"
+        url = "https://www.google.com/search?q=weather"
         webbrowser.get().open(url)
-        engine_speak("Here is what I found on google")
-    
-    #open gmail
-    if there_exists(["open my mail","gmail","check my email"]):
+        speak_text("Here is what I found on weather.")
+
+    # 16: open Gmail
+    if there_exists(["open my mail", "Gmail", "check my email"], voice_data):
         search_term = voice_data.split("for")[-1]
-        url="https://mail.google.com/mail/u/0/#inbox"
+        url = "https://mail.google.com/mail/u/0/#inbox"
         webbrowser.get().open(url)
-        engine_speak("here you can check your gmail")
-    
+        speak_text("Here you can check your Gmail.")
 
-    #10 stone paper scisorrs
-    
-    if there_exists(["game"]):
-        voice_data = record_audio("choose among rock paper or scissor")
-        moves=["rock", "paper", "scissor"]
-    
-        cmove=random.choice(moves)
-        pmove=voice_data
-        
+    # 17: game - rock paper scissor
+    if there_exists(["game"], voice_data):
+        voice_data = record_audio("Choose among rock, paper, or scissor.")
+        moves = ["rock", "paper", "scissor"]
+        cmove = random.choice(moves)
+        pmove = voice_data
+        speak_text("The computer chose " + cmove)
+        speak_text("You chose " + pmove)
+        if pmove == cmove:
+            speak_text("The match is a draw.")
+        elif pmove == "rock" and cmove == "scissor":
+            speak_text("Player wins.")
+        elif pmove == "rock" and cmove == "paper":
+            speak_text("Computer wins.")
+        elif pmove == "paper" and cmove == "rock":
+            speak_text("Player wins.")
+        elif pmove == "paper" and cmove == "scissor":
+            speak_text("Computer wins.")
+        elif pmove == "scissor" and cmove == "paper":
+            speak_text("Player wins.")
+        elif pmove == "scissor" and cmove == "rock":
+            speak_text("Computer wins.")
 
-        engine_speak("The computer chose " + cmove)
-        engine_speak("You chose " + pmove)
-        #engine_speak("hi")
-        if pmove==cmove:
-            engine_speak("the match is draw")
-        elif pmove== "rock" and cmove== "scissor":
-            engine_speak("Player wins")
-        elif pmove== "rock" and cmove== "paper":
-            engine_speak("Computer wins")
-        elif pmove== "paper" and cmove== "rock":
-            engine_speak("Player wins")
-        elif pmove== "paper" and cmove== "scissor":
-            engine_speak("Computer wins")
-        elif pmove== "scissor" and cmove== "paper":
-            engine_speak("Player wins")
-        elif pmove== "scissor" and cmove== "rock":
-            engine_speak("Computer wins")
+    # 18: toss a coin
+    if there_exists(["toss", "flip", "coin"], voice_data):
+        moves = ["head", "tails"]
+        cmove = random.choice(moves)
+        speak_text("The computer chose " + cmove)
 
-    #11 toss a coin
-    if there_exists(["toss","flip","coin"]):
-        moves=["head", "tails"]   
-        cmove=random.choice(moves)
-        engine_speak("The computer chose " + cmove)
-
-    # 12 calc
-    if there_exists(["plus","minus","multiply","divide","modulus","power","+","-","*","/","%","**"]):
+    # 19: calculator
+    if there_exists(["plus", "minus", "multiply", "divide", "modulus", "power", "+", "-", "*", "/", "%", "**"], voice_data):
         opr = voice_data.split()[1]
 
         if opr == '+':
-            engine_speak(int(voice_data.split()[0]) + int(voice_data.split()[2]))
+            speak_text(int(voice_data.split()[0]) + int(voice_data.split()[2]))
         elif opr == '-':
-            engine_speak(int(voice_data.split()[0]) - int(voice_data.split()[2]))
+            speak_text(int(voice_data.split()[0]) - int(voice_data.split()[2]))
         elif opr == '*':
-            engine_speak(int(voice_data.split()[0]) * int(voice_data.split()[2]))
+            speak_text(int(voice_data.split()[0]) * int(voice_data.split()[2]))
         elif opr == '/':
-            engine_speak(int(voice_data.split()[0]) / int(voice_data.split()[2]))
+            speak_text(int(voice_data.split()[0]) / int(voice_data.split()[2]))
         elif opr == '%':
-            engine_speak(int(voice_data.split()[0]) % int(voice_data.split()[2]))
+            speak_text(int(voice_data.split()[0]) % int(voice_data.split()[2]))
         elif opr == '**':
-            engine_speak(int(voice_data.split()[0]) ** int(voice_data.split()[2]))
+            speak_text(int(voice_data.split()[0]) ** int(voice_data.split()[2]))
         else:
-            engine_speak("Wrong Operator")
-        
-    #13 screenshot
-    if there_exists(["capture","my screen","screenshot"]):
+            speak_text("Wrong Operator")
+
+    # 20: take a screenshot
+    if there_exists(["capture", "my screen", "screenshot"], voice_data):
         myScreenshot = pyautogui.screenshot()
-        myScreenshot.save('/home/windingshelf/Desktop') 
-    
-    
-    #14 to search wikipedia for definition
-    if there_exists(["definition of"]):
-        definition=record_audio("what do you need the definition of")
-        url=urllib.request.urlopen('https://en.wikipedia.org/wiki/'+definition)
-        soup=bs.BeautifulSoup(url,'lxml')
-        definitions=[]
-        for paragraph in soup.find_all('p'):
-            definitions.append(str(paragraph.text))
-        if definitions:
-            if definitions[0]:
-                engine_speak('im sorry i could not find that definition, please try a web search')
-            elif definitions[1]:
-                engine_speak('here is what i found '+definitions[1])
-            else:
-                engine_speak ('Here is what i found '+definitions[2])
-        else:
-                engine_speak("i m sorry i could not find the definition for "+definition)
-
-
-    if there_exists(["exit", "quit", "mute", "goodbye"]):
-        engine_speak("we could continue more, but... byee")
-        exit()
-
-
-time.sleep(1)
+        myScreenshot.save('screenshot.png')
 
 person_obj = person()
 asis_obj = asis()
 asis_obj.name = 'ETERNITY'
-engine = pyttsx3.init()
 
+def main():
+    while True:
+        voice_data = record_audio("Recording")  # Get the voice input
+        respond(voice_data)
 
-while(1):
-    voice_data = record_audio("Recording") # get the voice input
-    print("Done")
-    print("Q:", voice_data)
-    respond(voice_data) # respond
+if __name__ == "__main__":
+    main()
